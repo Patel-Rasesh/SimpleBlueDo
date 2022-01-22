45b01a8240dc35d00eba5f64143dab444099fab9
ColinTrive <ColinTrive@users.noreply.github.com>
2022-01-22 15:03:36 -0600

    Initial commit

---
+++ app/src/main/java/com/example/simplebluedo/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.simplebluedo
+
+import android.hardware.biometrics.BiometricManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.FileUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
+
+class MainActivity : AppCompatActivity() {
+    var listOfTasks = mutableListOf<String>()
+    lateinit var adapter : TaskItemAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            Log.i("Clon", "User has clicked this!")
+//        }
+//        listOfTasks.add("Complete Pre-work")
+//        listOfTasks.add("Soak beans")
+
+        val onLongClickListener = object : TaskItemAdapter.OnLongClickListener{
+            override fun itemLongClicked(position: Int) {
+                // Move the item from the list
+                listOfTasks.removeAt(position)
+                // Notify the adapter
+                adapter.notifyDataSetChanged()
+                saveItems()
+            }
+        }
+        loadItems()
+        // Lookup the recyclerview in activity layout
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        // Create adapter passing in the sample user data
+        adapter = TaskItemAdapter(listOfTasks, onLongClickListener)
+        // Attach the adapter to the recyclerview to populate items
+        recyclerView.adapter = adapter
+        // Set layout manager to position the items
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val inputTextField = findViewById<EditText>(R.id.addTaskField)
+        // Finding the id of Add Task
+        findViewById<Button>(R.id.button).setOnClickListener{
+            // Store the text of the task
+            val userInputTask = inputTextField.text.toString()
+
+            // Persist the task in listOfTasks
+            listOfTasks.add(userInputTask)
+            // Notify adapter
+            adapter.notifyItemInserted(listOfTasks.size - 1)
+
+            // Reset the text field
+            inputTextField.setText("")
+            saveItems()
+        }
+    }
+    // Persist user inputs
+    // Create or Read
+    fun getDataFile() : File {
+        return File(filesDir, "data.txt")
+    }
+
+    // Load
+    fun loadItems(){
+        try {
+            listOfTasks = org.apache.commons.io.FileUtils.readLines(getDataFile(), Charset.defaultCharset())
+        }catch (ioException : IOException){
+            ioException.printStackTrace()
+        }
+    }
+
+    // Write
+    fun saveItems(){
+        try{
+            org.apache.commons.io.FileUtils.writeLines(getDataFile(), listOfTasks)
+        }catch (ioException : IOException){
+            ioException.printStackTrace()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/simplebluedo/TaskItemAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.simplebluedo
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * Adapters are view orchestrator
+ */
+class TaskItemAdapter (val listOfItems : List<String>,
+                       val longClickListener: OnLongClickListener) :
+    RecyclerView.Adapter<TaskItemAdapter.ViewHolder>(){
+
+    interface OnLongClickListener{
+        fun itemLongClicked(position : Int)
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+
+        // Inflate the custom layout
+        val contactView = inflater.inflate(android.R.layout.simple_list_item_1, parent, false)
+
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    // Involves populating data into the item through holder
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        // Get the data model based on position
+        val item = listOfItems.get(position)
+        holder.textView.text = item
+    }
+
+    override fun getItemCount(): Int {
+
+        // Returns total of list of items
+        return listOfItems.size
+    }
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView : TextView
+        init{
+            textView = itemView.findViewById(android.R.id.text1)
+            itemView.setOnLongClickListener{
+                longClickListener.itemLongClicked(adapterPosition)
+                true
+            }
+        }
+    }
+}
